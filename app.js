2ebf78d7ee52e79f176bba5826ff39922477ba5c
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-05-15 16:05:51 -0700

    made averages

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 //Object for location
-//we have a location
 var location = {
   //name of the place
-  name: 'FirstAndPike'
+  name: 'FirstAndPike',
   //list of the mins for all hours for the location
   min: 23,
   //list of the maxs for all hours for the location
   max: 65,
+  //this is their average cookie sales for the whole day usually
   regCookieSales: 6.3,
-  //function called averagePeoplePerHour, where min/max[i] are the parameters
+  //function called averagePeoplePerHour
   averagePeoplePerHour: function () {
-    math.random((this.max - this.min) +this.min);
-    //console log the result of the function
-    console.log(averagePeoplePerHour);
-    //console log the iteration# (0 will be 6am)
-    console.log(i);
-    }
-  // averageCookiesPerHour: function (this.) //<-- repeat above except with cookies
-  // }
+    //we'll take the max + min and divide by 2 to find the average - then multiply it by math.random to generate a random number that's rounded down.
+    //to get a random number between 65 and 23, we have to set 23 as 0 -so we add the minimum so we always have at least 23.
+    math.floor(math.random()*(max - min) + min));
+    return averagePeoplePerHour;
+  },
+  averageCookieSales: function () {
+    console.log(regCookieSales * averagePeoplePerHour());
+  },
+};
 
-//to call the function, use location.averagePeoplePerHour();
+//at six am, they need a random number of people
+
+
+//to call a function example: location.averagePeoplePerHour();
 
 //blank array
 var arr = [];
