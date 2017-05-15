09a9934b453a2c27906ecf99151542381617cbee
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-05-15 16:41:18 -0700

    added peopleAtHour and cookiesAtHour

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Object for location
 var location = {
   //name of the place
-  name: 'FirstAndPike',
+  name: 'First And Pike',
   //list of the mins for all hours for the location
   min: 23,
   //list of the maxs for all hours for the location
@@ -9,23 +9,33 @@ var location = {
   //this is their average cookie sales for the whole day usually
   regCookieSales: 6.3,
   //function called averagePeoplePerHour
-  averagePeoplePerHour: function () {
+  randomCrowd: function () {
     //we'll take the max + min and divide by 2 to find the average - then multiply it by math.random to generate a random number that's rounded down.
     //to get a random number between 65 and 23, we have to set 23 as 0 -so we add the minimum so we always have at least 23.
-    math.floor(math.random()*(max - min) + min));
-    return averagePeoplePerHour;
+    Math.floor(Math.random() * (this.max - this.min)) + this.min;
+    //show us the average # per hour
   },
+  //Averrage cookie sales function
   averageCookieSales: function () {
-    console.log(regCookieSales * averagePeoplePerHour());
+    //log to the console their regular sales * the random people per hour
+    console.log(this.regCookieSales * this.randomCrowd());
   },
+  //hours they are open in military time
+  hours: [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+  //cookies sold at each hour
+  peopleAtHour: function () {
+    for (var i=this.hours[0]; i<this.hours[19]; i++) {
+      console.log('# people was: ' +this.randomCrowd);
+    }
+  },
+  cookiesAtHour: function () {
+    for (var i=this.hours[0]; i<this.hours[19]; i++) {
+      console.log(' number of cookies sold was: '+ this.averageCookieSales);
+    }
+  // cookiesSoldToday: function () {
+  //   var soldToday = 0
+  }
+  }
 };
 
-//at six am, they need a random number of people
-
-
-//to call a function example: location.averagePeoplePerHour();
-
-//blank array
-var arr = [];
-var container = document.createElement('div');
-container.innerHTML = '<p>'
+location();
